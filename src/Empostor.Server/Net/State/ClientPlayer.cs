b8dc5accd0ea0b895b3778ddb8588754cccc5817
linmeideli <2558527373@qq.com>
2026-08-15 02:45:51 +0800

    a weird issue

--- src/Empostor.Server/Net/State/ClientPlayer.cs
+++ src/Empostor.Server/Net/State/ClientPlayer.cs
@@ -73,7 +73,7 @@ namespace Empostor.Server.Net.State
         {
             try
             {
-                if (Character == null)
+                if (Character == null && Game.GameState != GameStates.NotStarted)
                 {
                     _logger.LogInformation("{Code} - Player {Id} spawn timed out, kicking.", Game.Code, Client.Id);
                     await KickAsync();
